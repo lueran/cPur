5eb07033d50b814cc92e3d25f64acd8d0c06ece7
Eran Lunenfeld <eranl@paykey.com>
2018-05-23 19:15:36 +0300

    added login logic

--- app/src/main/java/com/cpur/LoginActivity.java
+++ app/src/main/java/com/cpur/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.cpur;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,21 +26,67 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final FirebaseAuth auth = FirebaseAuth.getInstance();
-        auth.createUserWithEmailAndPassword("lueran@gmail.com", "123456")
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        final EditText emailEditText = findViewById(R.id.email);
+        final EditText passwordEditText = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.getText().toString(),
+                        passwordEditText.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this,
+                                new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            Log.d(TAG, "signInWithEmail:success");
+                                            auth();
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        }
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = auth.getCurrentUser();
-                            Log.d(TAG, "User: "+ user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                        }
+                    public void onFailure(@NonNull Exception e) {
+
                     }
                 });
+            }
+        });
+
+        findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Signup code
+                final FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.createUserWithEmailAndPassword("test@mail.com", "123456")
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    Log.d(TAG, "User: " + user);
+                                    auth();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                }
+                            }
+                        });
+            }
+        });
+
+
+    }
+
+    private void auth(){
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
     }
 }
---
+++ app/src/main/java/com/cpur/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.cpur;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        Intent startIntent;
+        if (auth.getCurrentUser() != null) {
+            startIntent = new Intent(this, MainActivity.class);
+        } else {
+            startIntent = new Intent(this, LoginActivity.class);
+        }
+
+        startActivity(startIntent);
+    }
+}
